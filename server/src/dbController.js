0c6367fbb12f89f849d39686398d03214433287c
Leena WH <wolee111@coupang.com>
2022-01-30 01:56:52 +0900

    add db controller

---
+++ server/src/dbController.js
@@ -0,0 +1,25 @@
+// package.json에서 type: module을 추가하지않으면 require 문법을 사용해야한다.
+import fs from 'fs'; // nodejs에 있는 file system
+import {resolve} from 'path';
+
+const basePath = resolve() // 현재 위치
+const filenames = {
+    message: resolve(basePath, 'src/db/messages.json'),
+    users: resolve(basePath, 'src/db/users.json')
+}
+
+export const readDB = target => {
+    try{
+        return JSON.parse(fs.readFileSync(filenames[target], 'utf-8'))
+    }catch (e){
+        console.error(e)
+    }
+}
+
+export const writeDB = (target, data) => {
+    try{
+        return fs.writeFileSync(filenames[target], JSON.stringify(data))
+    }catch (e) {
+        console.error(e)
+    }
+}
