576996c6f3920ca1dcb408e221efa64e4bc048e8
Leena WH <wolee111@coupang.com>
2022-01-31 21:25:05 +0900

    add fetcher

--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -1,58 +1,75 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import { useRouter } from "next/router";
 import MsgItem from "./MsgItem";
 import MsgInput from "./MsgInput";
-
-const UserIds = ['roy', 'jay']
-const getRandomUserId = () => UserIds[Math.round(Math.random())]
-const defaultMsgs = Array(50).fill(null).map((_,i) => ({
-    id: i+1,
-    userId: getRandomUserId(),
-    timestamp: 1234567890123 + (50 - i) * 1000 * 60, // 50-i : 시간 내림차순 정렬을 위해
-    text: `${i+1} mock text`
-}))
+import fetcher from '../fetcher'
 
 const MsgList = () => {
-    const [msgs, setMsgs] = useState(defaultMsgs)
+    const { query: { userId = '' } } = useRouter();
+    const [msgs, setMsgs] = useState([])
     const [editingId, setEditingId] = useState(null)
-    const onCreate = text => {
-        const newMsg = {
-            id: msgs.length + 1,
-            userId: getRandomUserId(),
-            timestamp: Date.now(),
-            text: `${msgs.length + 1} ${text}`
-        }
+    const onCreate = async text => {
+        const newMsg = await fetcher('post', '/messages', {
+            text,
+            userId,
+        })
+        if(!newMsg) throw Error('create failed')
         setMsgs(msgs => [newMsg, ...msgs])
     }
-    const onUpdate = (text, id) => {
+    const onUpdate = async (text, id) => {
+        const newMsg = await fetcher('put', `/messages/${id}`, {
+            text,
+            userId,
+        })
+        if(!newMsg) throw Error('update failed')
         setMsgs(msgs => {
             // 밖에서 state를 가져오기보단 setState를 함수형으로 활용하는게 안정적으로 확실한 msgs를 가져올 수 있다.
             const targetIndex = msgs.findIndex(msg => msg.id === id);
             if(targetIndex < 0) return msgs;
             const newMsgs = [...msgs]
-            newMsgs.splice(targetIndex, 1, {
-                ...msgs[targetIndex],
-                text
-            })
+            newMsgs.splice(targetIndex, 1, newMsg)
             return newMsgs
         })
         doneEdit()
     }
     const doneEdit = () => setEditingId(null)
-    const onDelete = (id) => {
+    const onDelete = async (id) => {
+        const deletedId = await fetcher('delete', `/messages/${id}`, {
+            params: { userId },
+        })
+        if(!deletedId) throw Error('update failed')
         setMsgs(msgs => {
-            const targetIndex = msgs.findIndex(msg => msg.id === id);
+            const targetIndex = msgs.findIndex(msg => msg.id === deletedId+'');
             if(targetIndex < 0) return msgs;
             const newMsgs = [...msgs]
-            newMsgs.splice(targetIndex, 1, )
+            newMsgs.splice(targetIndex, 1)
             return newMsgs
         })
     }
+
+    const getMessages = async () => {
+        const messages = await fetcher('get', '/messages')
+        setMsgs(messages)
+    }
+
+    useEffect(() => {
+        getMessages();
+    }, [])
+
     return (
         <>
             <MsgInput mutate={onCreate}/>
             <ul className="messages">
                 {
-                    msgs.map((value,i) => <MsgItem key={i} onUpdate={onUpdate} startEdit={() => setEditingId(value.id)} isEditing={editingId === value.id} onDelete={() => onDelete(value.id)} {...value}/>)
+                    msgs.map((value,i) =>
+                        <MsgItem
+                            key={i}
+                            onUpdate={onUpdate}
+                            onDelete={() => onDelete(value.id)}
+                            startEdit={() => setEditingId(value.id)}
+                            isEditing={editingId === value.id}
+                            currentUserId={userId}
+                            {...value}/>)
                 }
             </ul>
         </>
--- server/src/routes/messages.js
+++ server/src/routes/messages.js
@@ -71,12 +71,12 @@ const messagesRoute = [
         // DELETE message
         method: 'delete',
         route: '/messages/:id',
-        handler({ body, params: { id } }, res) {
+        handler({ body, params: { id }, query: { userId } }, res) {
             try {
                 const msgs = getMessages();
                 const targetIndex = msgs.findIndex(msg => msg.id === id)
                 if(targetIndex < 0) throw '메시지가 없습니다.';
-                if(msgs[targetIndex].userId !== body.userId) throw '사용자가 다릅니다.'
+                if(msgs[targetIndex].userId !== userId) throw '사용자가 다릅니다.'
 
                 msgs.splice(targetIndex, 1)
                 setMessages(msgs)
